8b18e2755c5d4f7f675b216f909fd79cee1fe565
thandon263 <ncubethando20@gmail.com>
2018-01-23 03:23:24 -0500

    updating the keys.js file

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -45429,11 +45429,12 @@ function stop(id) {
 
 /* WEBPACK VAR INJECTION */(function(process) {// Keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
-  // we are in production - return the prod set of keys
-  module.exports = __webpack_require__(194);
-} else {
   // we are in development - return the dev set of keys!!
   module.exports = __webpack_require__(195);
+} else {
+  // we are in production - return the prod set of keys
+
+  module.exports = __webpack_require__(194);
 }
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4)))
 
